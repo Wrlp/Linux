a94f9a0fbcfe614412acbe73a89c15d46c10b87b
Wrlp <laure.warlop@gmail.com>
2026-04-12 15:26:42 -0400

    hypr

--- script/screenshots.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-grim -g "$(slurp)" "$HOME/screenshot/screenshot.png" && wl-copy < $HOME/screenshot/screenshot.png && notify-send "Capture" "Copiée dans le presse-papier" && imv $HOME/screenshot/screenshot.png &
