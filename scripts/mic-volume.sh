07c9d3526c6cb811f2fddc9abd2b6535aee72752
Wrlp <laure.warlop@gmail.com>
2026-06-25 11:42:24 -0400

    update mic-volume

--- scripts/mic-volume.sh
+++ scripts/mic-volume.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-while true; do
+pactl subscribe | grep --line-buffered "source" | while read -r event; do
+    sleep 0.5
     wpctl set-volume @DEFAULT_SOURCE@ 0.25
-    sleep 2
 done
